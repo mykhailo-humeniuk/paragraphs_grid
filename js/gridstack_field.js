41f5011543fc921ed56f61f3f2f38e156a793d94
Misha Gumeniuk <odyssey@internetdevels.com>
2018-02-20 08:18:37 +0200

    small code refactoring

--- js/gridstack_field.js
+++ js/gridstack_field.js
@@ -34,11 +34,11 @@
    */
   Drupal.behaviors.gridstackField = {
     attach: function (context, settings) {
-      let $body = $('body');
+      const $body = $('body');
       let input = '';
       let data = '';
-      let $fieldGridstack = $('.form-item-grid');
-      let options = {
+      const $fieldGridstack = $('.form-item-grid');
+      const options = {
         alwaysShowResizeHandle: false,
         animate: false,
         cellHeight: '50px',
@@ -46,63 +46,34 @@
         width: 12,
         float: false
       };
-      let myStorage = window.localStorage;
   
       $fieldGridstack.gridstack(options);
 
       // Fill in JSON field with parameters from grid items.
       (function () {
-        let $grid_container = $('.form-item-grid');
+        const $grid_container = $('.form-item-grid');
         let $grid_items = $grid_container.find('.grid-stack-item.ui-draggable.ui-resizable');
-        let myStorage = window.localStorage;
-        let jsonFieldData = (myStorage.getItem('gridItems')) ? JSON.parse(myStorage.getItem('gridItems')) : [];
+        let jsonFieldData = [];
         
         // Fill in cache if field with json is not empty.
-        if (jsonFieldData.length || $grid_items.length) {
-          if ($grid_items.length === jsonFieldData.length) {
-            _saveParagraphPosition(jsonFieldData);
-          }
-          else {
-            // Synchronize value in kson field and items in grid then save them in cache.
-            jsonFieldData = [];
-            $grid_items.each(function (key, item) {
-              var obj = {
-                x: $(item).data('gs-x'),
-                y: $(item).data('gs-y'),
-                width: $(item).data('gs-width'),
-                height: $(item).data('gs-height'),
-                delta: $(item).data('delta')
-              };
-              jsonFieldData.push(obj);
-            });
-            _saveParagraphPosition(jsonFieldData);
-          }
-        }
-
-        // Fill in and save data about new item.
-        // TODO need to try to refactor this.
-        if (context[0] !== undefined && $(context[0].parentNode).hasClass('field-name-field-grid')) {
-          var $new_item =  $(context[0].innerHTML).find('.ajax-new-content').closest('.grid-stack-item.ui-draggable.ui-resizable');
-          if ($new_item.length) {
+        if ($grid_items.length) {
+          $grid_items.each(function (key, item) {
             var obj = {
-              x: $new_item.data('gs-x'),
-              y: $new_item.data('gs-y'),
-              width: $new_item.data('gs-width'),
-              height: $new_item.data('gs-height'),
-              delta: $new_item.data('delta')
+              x: $(item).data('gs-x'),
+              y: $(item).data('gs-y'),
+              width: $(item).data('gs-width'),
+              height: $(item).data('gs-height'),
+              delta: $(item).data('delta')
             };
-            // jsonFieldData.push(obj);
-            if (typeof jsonFieldData[$new_item.data('delta')] == 'undefined') {
-              jsonFieldData.push(obj);
-            }
-            _saveParagraphPosition($field, jsonFieldData);
-          }
+            jsonFieldData.push(obj);
+          });
+          _saveParagraphPosition(jsonFieldData);
         }
 
         $('.field-widget-paragraphs-gridstack').once('save-item', function () {
           // Add custom element with value of item height.
           $grid_items.each(function (key, item) {
-            var height = $(item).data('gs-height');
+            let height = $(item).data('gs-height');
             height = 'Height: ' + (height * 50) + 'px';
             $(item).find('.grid-stack-item-content').prepend('<div class="height-counter">' + height + '</div>');
           });
@@ -118,7 +89,7 @@
                   delta: this.el[0].dataset.delta
                 };
 
-                if ((jsonFieldData.length == 0) || (items[i].el[0].dataset.delta == jsonFieldData.length)) {
+                if ((jsonFieldData.length === 0) || (items[i].el[0].dataset.delta === jsonFieldData.length)) {
                   jsonFieldData.push(obj);
                 }
                 else {
@@ -134,7 +105,6 @@
                 height = 'Height: ' + (height * 50) + 'px';
                 $height_container.text(height);
               });
-
               _saveParagraphPosition(jsonFieldData);
             }
           });
